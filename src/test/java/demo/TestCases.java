b9e111b948ef3abdbd02dd7f64b21a30a967d8e3
gorv001 <qagauravrana@gmail.com>
2024-06-07 13:37:19 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -8,48 +8,27 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
 import java.time.Duration;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
-
-
-import java.util.List;
 import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
+
 import demo.wrappers.Wrappers;
-import dev.failsafe.internal.util.Durations;
 
 public class TestCases {
-    ChromeDriver driver;
-
-    /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
-     */
-
-     
-    /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
-     */
+    private ChromeDriver driver;
+    private static final String URL = "https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform";
+    
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
-
-        // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
 
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
-
         logs.enable(LogType.BROWSER, Level.ALL);
         logs.enable(LogType.DRIVER, Level.ALL);
         options.setCapability("goog:loggingPrefs", logs);
@@ -58,45 +37,41 @@ public class TestCases {
         System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
 
         driver = new ChromeDriver(options);
-
         driver.manage().window().maximize();
     }
 
     @Test
-    public void testCase01() throws InterruptedException{  
+    public void testCase01() throws InterruptedException {  
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        Wrappers.navigatToUrl(driver);
+        Wrappers.navigateToUrl(driver, URL);
         Wrappers.verifyThePage(driver, "forms");
-        Thread.sleep(1000);
+
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        WebElement NameElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class=\"rFrNMe k3kHxc RdH0ib yqQS1 zKHdkd\"]/div/div/div/input")));
-        Wrappers.sendText(NameElement, "Crio Learner");
-        Wrappers.verifyText(NameElement, "Crio Learner");
+        
+        WebElement nameElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='rFrNMe k3kHxc RdH0ib yqQS1 zKHdkd']/div/div/div/input")));
+        Wrappers.sendText(nameElement, "Crio Learner");
+        Wrappers.verifyText(nameElement, "Crio Learner");
+        
         WebElement practicingAutomation = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//textarea[@class='KHxj8b tL9Q4c']")));
         Wrappers.sendTextWithTime(practicingAutomation, "I want to be the best QA Engineer!");
-        Wrappers.verifyText(practicingAutomation,"I want to be the best QA Engineer!");
-        Wrappers.addexperience(driver,"3 - 5");
-        Wrappers.selectTools(driver,"Java");
-        Wrappers.selectTools(driver,"Selenium");
-        Wrappers.selectTools(driver,"TestNG");
-        WebElement addressElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='MocG8c HZ3kWc mhLiyf LMgvRb KKjvXb DEh1R']")));
-        addressElement.click();
-        Thread.sleep(1000);
-        Wrappers.selectAdress(driver, "Mr");
-        Thread.sleep(1000);
-        Wrappers.date(driver);
-        Thread.sleep(1000);
-        Wrappers.time(driver);
+        Wrappers.verifyText(practicingAutomation, "I want to be the best QA Engineer!");
+        Wrappers.addExperience(driver, "3 - 5");
+        Wrappers.selectTools(driver, "Java");
+        Wrappers.selectTools(driver, "Selenium");
+        Wrappers.selectTools(driver, "TestNG");
+        Wrappers.selectAddress(driver, "Mr");
+        Thread.sleep(2000);
+        Wrappers.enterDate(driver);
+        Wrappers.enterTime(driver);
         Wrappers.clickOnSubmitButton(driver);
         Thread.sleep(1000);
         Wrappers.successMessage(driver);
     }
 
     // @AfterTest
-    // public void endTest()
-    // {
-    //     driver.close();
-    //     driver.quit();
-
+    // public void endTest() {
+    //     if (driver != null) {
+    //         driver.quit();
+    //     }
     // }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,236 +1,140 @@
 package demo.wrappers;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import dev.failsafe.internal.util.Durations;
-
-import java.time.Duration;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class Wrappers {
-    
-    /*
-     * Write your selenium wrappers here
-     */
-   static String url = "https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform";
-  
-     public static boolean navigatToUrl(WebDriver driver){
-        try {
-            if(driver.getCurrentUrl().equals(url)){
-                System.out.println("Already at the desired Page");
-                return true;
-            }else{
-                driver.get(url);
-                System.out.println("Navigated to the google forms Page");
-                return driver.getCurrentUrl()==url;
-            }
-        } catch (Exception e) {
-            System.out.println("Exception Occured");
-            e.getStackTrace();
-            return false;
-        }
 
-     }
+    public static void navigateToUrl(WebDriver driver, String url) {
+        driver.get(url);
+        System.out.println("Navigated to the google forms Page");
+    }
 
-     public static boolean verifyThePage(WebDriver driver, String PageName){
-
-        try {
-            if(driver.getCurrentUrl().contains(PageName)){
-                System.out.println("Page is verified");
-                return true;
-            }else{
-                System.out.println("Page is not verified");
-                return false;
-            }
-            
-        } catch (Exception e) {
-            System.out.println("Exception Occured");
-            e.getStackTrace();
-            return false;
-        }
-     }
-
-     public static boolean advanceClick(WebDriver driver, WebElement element){
-        try {
-            if(element.isDisplayed()){
-                if(element.isEnabled()){
-                    JavascriptExecutor js = ((JavascriptExecutor)driver);
-                    js.executeAsyncScript("arguments[0].scrollIntoView(true);", element);
-                    element.click();
-                    System.out.println("Element is clicked successfully");
-                    return true;
-                }else{
-                    System.out.println("Element is not Enabled");
-                    return false;
-                }
-            }else{
-                System.out.println("Element is not Displayed");
-                return false;
-            }
-        } catch (Exception e) {
-            return false;
-        }
-     }
-
-     public static boolean sendText(WebElement element, String sendKey){
-         try {
-            element.clear();
-            element.sendKeys(sendKey);
-            System.out.println("Crio Learner is filled Successfully");
-            return true;
-         } catch (Exception e) {
-            System.out.println("Exception Occured");
-            e.getStackTrace();
-            return false;
-         }
-     }
-
-     public static boolean verifyText(WebElement element, String text){
-        try {
-            if(element.getAttribute("data-initial-value").contains(text)){
-                System.out.println(element.getAttribute("data-initial-value"));
-                System.out.println("Text is verified Succefully");
-                return true;
-            }else{
-                System.out.println("Text is not verified");
-                return false;
-            }
-        } catch (Exception e) {
-            System.out.println("Exception Occured");
-            e.getStackTrace();
-            return false;
-        }
-     }
-
-    public static boolean sendTextWithTime(WebElement practicingAutomation, String text) {
-        // TODO Auto-generated method stub
-        try {
-            long timeStamp = System.currentTimeMillis()/1000;
-            practicingAutomation.sendKeys(text+timeStamp);
-            return true;
-        } catch (Exception e) {
-            System.out.println("Exception Occured");
-            e.getStackTrace();
-            return false;
+    public static void verifyThePage(WebDriver driver, String pageName) {
+        if (driver.getCurrentUrl().contains(pageName)) {
+            System.out.println("Page is verified");
+        } else {
+            System.out.println("Page is not verified");
         }
     }
 
-    public static void addexperience(WebDriver driver,String n) {
-       try {
-        List<WebElement> experinceElements = driver.findElements(By.xpath("//div[@class=\"nWQGrd zwllIb\"]/label/div/div[2]/div/span"));
-        for(WebElement e : experinceElements){
-            if(e.getText().contains(n)){
-                e.click();
-                System.out.println("Experince is selected succesfully");
-                Thread.sleep(1000);
-            }
-               
+    public static void clickElement(WebElement element) {
+        if (element.isDisplayed() && element.isEnabled()) {
+            element.click();
+        } else {
+            System.out.println("Element is not clickable");
         }
-       } catch (Exception e) {
-        System.out.println("Exception Occured");
-        e.getStackTrace();
-       }
+    }
+
+    public static void sendText(WebElement element, String text) {
+        element.clear();
+        element.sendKeys(text);
+        System.out.println(text + " is filled successfully");
+    }
+
+    public static void verifyText(WebElement element, String text) {
+        if (element.getAttribute("data-initial-value").contains(text)) {
+            System.out.println("Text is verified successfully");
+        } else {
+            System.out.println("Text is not verified");
+        }
+    }
+
+    public static void sendTextWithTime(WebElement element, String text) {
+        long timeStamp = System.currentTimeMillis() / 1000;
+        element.sendKeys(text + timeStamp);
+        System.out.println("Text with timestamp is filled successfully");
+    }
+
+    public static void addExperience(WebDriver driver, String experience) {
+        List<WebElement> experienceElements = driver.findElements(By.xpath("//div[@class='nWQGrd zwllIb']/label/div/div[2]/div/span"));
+        for (WebElement element : experienceElements) {
+            if (element.getText().contains(experience)) {
+                clickElement(element);
+                System.out.println("Experince is selected Succesfully");
+                break;
+            }
+        }
     }
 
     public static void selectTools(WebDriver driver, String tool) {
-        try {
-            List<WebElement> tools = driver.findElements(By.xpath("//span[@class='aDTYNe snByac n5vBHf OIC90c']"));
-            for(WebElement e: tools){
-                if(e.getText().contains(tool)){
-                    e.click();
-                    System.out.println(tool+" is selected Succesfully");
-                }
+        List<WebElement> tools = driver.findElements(By.xpath("//span[@class='aDTYNe snByac n5vBHf OIC90c']"));
+        for (WebElement element : tools) {
+            if (element.getText().contains(tool)) {
+                clickElement(element);
+                System.out.println(tool+" is selected Successfully");
+                break;
             }
-        } catch (Exception e) {
-            System.out.println("Exception Occured");
-            e.getStackTrace();
         }
     }
 
-    public static void selectAdress(WebDriver driver, String text) {
-       try {
-        List<WebElement> adressOptions = driver.findElements(By.xpath("//div[@class='OA0qNb ncFHed QXL7Te']/div/span"));
-        for(WebElement e: adressOptions){
-            if(e.getText().equalsIgnoreCase(text)){
-                e.click();
-                System.out.println("Adressed is selected sucessfully");
+    public static void selectAddress(WebDriver driver, String text) throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        WebElement addressElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='MocG8c HZ3kWc mhLiyf LMgvRb KKjvXb DEh1R']")));
+        addressElement.click();
+        Thread.sleep(1000);
+        List<WebElement> addressOptions = driver.findElements(By.xpath("//div[@class='OA0qNb ncFHed QXL7Te']/div/span"));
+        for (WebElement element : addressOptions) {
+            if (element.getText().equalsIgnoreCase(text)) {
+                clickElement(element);
+                break;
             }
         }
-       } catch (Exception e) {
-        System.out.println("Exception Occured");
-        e.getStackTrace();
-       }
     }
+    
 
-    public static void date(WebDriver driver) {
-        try {
-            Calendar calendar = Calendar.getInstance();
+    public static void enterDate(WebDriver driver) {
+        Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DAY_OF_YEAR, -7);
         Date previousDate = calendar.getTime();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-        String formatedDate = dateFormat.format(previousDate);
-        WebElement date = driver.findElement(By.xpath("(//input[@class='whsOnd zHQkBf'])[2]"));
-        date.sendKeys(formatedDate);
-        } catch (Exception e) {
-            System.out.println("Exception Occured");
-            e.getStackTrace();
-        }
+        String formattedDate = dateFormat.format(previousDate);
 
+        WebElement dateElement = driver.findElement(By.xpath("(//input[@class='whsOnd zHQkBf'])[2]"));
+        dateElement.sendKeys(formattedDate);
+        System.out.println("Date entered successfully: " + formattedDate);
     }
 
-    public static void time(WebDriver driver) {
-       try {
-        Calendar cal = Calendar.getInstance();
-        // int hours = cal.get(Calendar.HOUR);
-        // int minutes = cal.get(Calendar.MINUTE);
-        WebElement timeHr = driver.findElement(By.xpath("//input[@aria-label='Hour']"));
+    public static void enterTime(WebDriver driver) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOf(timeHr));
-        timeHr.sendKeys("07");
-        WebElement timeMin = driver.findElement(By.xpath("//input[@aria-label='Minute']"));
-        wait.until(ExpectedConditions.visibilityOf(timeMin));
-        timeMin.sendKeys("30");
-       } catch (Exception e) {
-        // TODO: handle exception
-       }
+        
+        WebElement hourElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@aria-label='Hour']")));
+        hourElement.sendKeys("07");
+
+        WebElement minuteElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@aria-label='Minute']")));
+        minuteElement.sendKeys("30");
+        
+        System.out.println("Time entered successfully: 07:30");
     }
 
     public static void clickOnSubmitButton(WebDriver driver) {
-        try {
-            WebElement SubmitButton = driver.findElement(By.xpath("//span[text()='Submit']"));
-        SubmitButton.click();
-        } catch (Exception e) {
-            System.out.println("Exception Occured");
-            e.getStackTrace();
-        }
+        WebElement submitButton = driver.findElement(By.xpath("//span[text()='Submit']"));
+        clickElement(submitButton);
     }
-   
 
-
+    
     public static void successMessage(WebDriver driver) {
-        WebElement getText = driver.findElement(By.xpath("//div[@class='vHW8K']"));
-        String text = getText.getText();
-        System.out.println(text);
-            if(text.contains("Thanks for your response")){
-            System.out.println("Got the Response");
-        }else{
-            System.out.println("Didn't get the Response");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        WebElement messageElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='vHW8K']")));
+        System.out.println("COMMAND: GetElementText");
+        String actualMessage = messageElement.getText();
+        System.out.println("RESPONSE: " + actualMessage);
+        if (messageElement.getText().contains("Thanks for your response")) {
+            System.out.println("Success message verified: Thanks for your response");
+        } else {
+            System.out.println("Success message not found");
         }
-
     }
+    
+    
 }
